3c1d3c02b459f925920ce4a304711817d34193c2
rjz <rj@rjzaworski.com>
2014-08-17 08:58:43 -0700

    Improves documentation task
      * Accept list of source files
      * Support for `group` parameter
      * Autolink to definitions in repository
      * Tidies up templates

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+
+/**
+ * A module to add two numbers
+ *
+ *     require('./index')(1, 3); // 4
+ *
+ * @id index
+ * @group index
+ * @type Function
+ * @param {Number} a - the first number
+ * @param {Number} b - the second number
+ */
 module.exports = function add (a, b) {
   return a + b;
 };
