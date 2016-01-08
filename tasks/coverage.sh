b8180e71072db7b0aca71f03fcd62357b17b74a9
rjz <rj@rjzaworski.com>
2016-01-07 21:06:40 -0800

    Breaks coverage task into stages
    Separating out the `cover` and `report` tasks enables report generation
    from multiple coverage files (e.g., if coverage is carried across
    clustered processes).

---
+++ tasks/coverage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NPMBINDIR="${DIR}/../node_modules/.bin"
+COVERAGEDIR="${DIR}/coverage"
+
+rm -rf "${COVERAGEDIR}"
+
+# Generate coverage files
+NODE_ENV=test ${NPMBINDIR}/istanbul cover \
+  ${NPMBINDIR}/_mocha test/ \
+  --dir="${COVERAGEDIR}" \
+  --print=none \
+  --report=none
+
+# Generate report
+${NPMBINDIR}/istanbul report lcov html text-summary \
+  --input="${COVERAGEDIR}"
+
